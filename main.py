1052db49878569e87e71505a73fd07639c993d94
Damjan Pavlovic <pavlovic.damjan1828@gmail.com>
2026-02-06 16:45:09 +0100

    Added Vercel URL to CORS

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http:''127.0.0.1:5173",
-        "https://*.vercel.app"           
+        "https://ai-agent-five-lake.vercel.app/"           
         ],
     allow_credentials=True,
     allow_methods=["*"],
