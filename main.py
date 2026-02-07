43fc5d3f77002fb06cf44cc0c59d1a2a2d044624
Damjan Pavlovic <pavlovic.damjan1828@gmail.com>
2026-02-07 11:16:56 +0100

    Fixed CORS

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http:''127.0.0.1:5173",
-        "https://ai-agent-five-lake.vercel.app/"           
-        ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
